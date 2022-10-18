ece49a95394e721e8e8c09a61b068c3560537aca
kkm5291 <kkm23125291@gmail.com>
2022-10-18 18:58:02 +0900

    first bubble review

--- src/chap06/BubbleSort2.java
+++ src/chap06/BubbleSort2.java
@@ -1,5 +1,7 @@
 package chap06;
 
+import java.util.Scanner;
+
 public class BubbleSort2 {
     static void swap(int[] a, int idx1, int idx2) {
         int t = a[idx1];
@@ -8,7 +10,7 @@ public class BubbleSort2 {
     }
 
 
-    static void bubbleSort(int[] a, int n) {
+    static void bubbleSort2(int[] a, int n) {
         for (int i = 0; i < n - 1; i++) {
             int exchg = 0;
             for (int j = n - 1; j > i; j--) {
@@ -18,6 +20,28 @@ public class BubbleSort2 {
             }
             if(exchg == 0)
                 break;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("버블 정렬(버전1)");
+
+        System.out.print("요솟수 : ");
+        int nx = sc.nextInt();
+        int[] x = new int[nx];
+
+        for (int i = 0; i < nx; i++) {
+            System.out.print("x[" + i + "] : ");
+            x[i] = sc.nextInt();
+        }
+
+        bubbleSort2(x, nx);
+
+        System.out.println("오름차순으로 정렬했습니다.");
+        for (int i = 0; i < nx; i++) {
+            System.out.println("x[" + i + "] = " + x[i]);
         }
     }
 }
