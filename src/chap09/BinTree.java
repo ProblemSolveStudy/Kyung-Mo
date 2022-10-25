af43a1f40ee6f01ebfd829e9fe0e7784afea9d0e
kkm5291 <kkm23125291@gmail.com>
2022-10-25 21:07:24 +0900

    tree

--- src/chap09/BinTree.java
+++ src/chap09/BinTree.java
@@ -7,11 +7,10 @@ public class BinTree<K, V> {
     static class Node<K, V> {
         private K key;
         private V data;
-
         private Node<K, V> left;
         private Node<K, V> right;
 
-        Node(K key, V data, Node<K, V> left, Node<K, V> right) {
+        public Node(K key, V data, Node<K, V> left, Node<K, V> right) {
             this.key = key;
             this.data = data;
             this.left = left;
@@ -31,21 +30,60 @@ public class BinTree<K, V> {
         }
     }
 
-    private Node<K, V> root;
+    private Node<K,V> root;
     private Comparator<? super K> comparator = null;
 
-    public BinTree() {
+    public BinTree() { // 자연순서로 키값 비교
         root = null;
     }
 
-    public BinTree(Comparator<? super K> c){
+    public BinTree(Comparator<? super K> c) { // 비교자로 키값을 비교함
         this();
         comparator = c;
     }
 
-    // 두 키값을 비교
     private int comp(K key1, K key2) {
-        return (comparator == null) ? ((Comparable<K>) key1).compareTo(key2) : comparator.compare(key1, key2);
+        return (comparator == null) ? ((Comparable<K>) key1).compareTo(key2)
+                : comparator.compare(key1, key2);
+    }
+
+    public V search(K key) {
+        Node<K,V> p = root;
+
+        while (true) {
+            if (p == null)
+                return null;
+            int cond = comp(key, p.getKey());
+            if(cond==0)
+                return p.getValue();
+            else if (cond < 0)
+                p = p.left;
+            else
+                p = p.right;
+        }
+    }
+
+    private void addNode(Node<K, V> node, K key, V data) {
+        int cond = comp(key, node.getKey());
+        if (cond == 0)
+            return;
+        else if (cond < 0) {
+            if(node.left == null)
+                node.left = new Node<K, V>(key, data, null, null);
+            else addNode(node.left, key, data);
+        } else {
+            if (node.right == null)
+                node.right = new Node<K, V>(key, data, null, null);
+            else
+                addNode(node.right, key, data);
+        }
+    }
+
+    public void add(K key, V data) {
+        if (root == null)
+            root = new Node<K, V>(key, data, null, null);
+        else
+            addNode(root, key, data);
     }
 
 }
