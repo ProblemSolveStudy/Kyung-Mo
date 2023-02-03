3e9ef756c7db0c2f7a425820867f13527ef527c7
kkm5291 <kkm23125291@gmail.com>
2023-02-03 10:24:09 +0900

    1874 : 이번엔 bufferedWriter까지 사용해서 써봤는데 print문보다 메모리도 더 많이 먹고 속도도 느렸음

--- src/baekjoonPs/B_1874.java
+++ src/baekjoonPs/B_1874.java
@@ -1,8 +1,6 @@
 package baekjoonPs;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Stack;
 
 /**
@@ -24,21 +22,20 @@ import java.util.Stack;
 
 public class B_1874 {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
 
         Stack<Integer> stack = new Stack<>();
 
-        int N = Integer.parseInt(br.readLine());
-
         int start = 0;
-
-        while (N-- > 0) {
-
+        while (n-- > 0) {
             int value = Integer.parseInt(br.readLine());
 
-            if (value > start) {
+            if (start < value) {
                 for (int i = start + 1; i <= value; i++) {
                     stack.push(i);
                     sb.append('+').append('\n');
@@ -46,15 +43,15 @@ public class B_1874 {
 
                 start = value;
             } else if (stack.peek() != value) {
-                System.out.println("NO");
+                bw.write("NO");
+                bw.flush();
                 return;
             }
 
-            // value 값이 들어오면 그 값을 꺼내기 위함!
             stack.pop();
             sb.append('-').append('\n');
         }
-
-        System.out.println(sb);
+        bw.write(String.valueOf(sb));
+        bw.flush();
     }
 }
